fecc2784f0a1168d981abf2f732a8100dc419e7b
Alex QA <etercity@gmail.com>
2021-08-12 10:10:30 +0300

    select browser (request, addoption)

--- conftest.py
+++ conftest.py
@@ -5,13 +5,33 @@ chrome_driver = './drivers/chromedriver.exe'
 firefox_driver = './drivers/geckodriver.exe'
 
 
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome(executable_path=chrome_driver)
+#     # browser = webdriver.Firefox(executable_path=firefox_driver) #для фокса
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(executable_path=chrome_driver)
-    # browser = webdriver.Firefox(executable_path=firefox_driver) #для фокса
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(executable_path=chrome_driver)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox(executable_path=firefox_driver)  # для фокса
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-
